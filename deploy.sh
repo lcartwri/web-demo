6423d151ec0b1b5e04c5384c323c1695e1cb7390
lcartwri <lcartwri@cisco.com>
2020-11-16 14:46:26 +0000

    changed travis script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,3 @@
-docker build -t lcartwri/frontend-test:latest -t lcartwri/frontend-test:$SHA -f .
-
-docker push lcartwri/frontend-test:latest
+docker build -t lcartwri/frontend-test:$SHA -f .
 
 docker push lcartwri/frontend-test:$SHA
